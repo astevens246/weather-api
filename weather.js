d2541da6a51bc0c0d030be9f41e30fb022b98696
Allen Stevens <allen.stevens@students.dominican.edu>
2024-06-17 16:40:59 -0700

    completed challenge 5

--- weather.js
+++ weather.js
@@ -1,15 +1,25 @@
  // Functions 
- function getWeather(apikey, zip, callback) {
+ async function getWeather(apikey, zip) {
     // Replace this with your own API key!
     const apiKey = '1c4e590826ba3ca40333880acb9b0ddd'
     const units = 'imperial'
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    fetch(path)
-      .then(res => res.json())
-      .then(json => {
-        callback(json);
-      })
-      .catch(err => console.log(err.message))
+    
+    const response = await fetch(path)
+    const data = await response.json()
+
+    // new object iwth more descriptive names
+    const weatherData = {
+      city: data.name,
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+    };
+
+    console.log(weatherData)
+    return weatherData;
+
   }
 
   export { getWeather }
