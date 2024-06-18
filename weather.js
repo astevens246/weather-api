427fd3647cc8fa19262e14c84754153cbdffcf83
Allen Stevens <allen.stevens@students.dominican.edu>
2024-06-17 17:02:15 -0700

    completed challenge 7

--- weather.js
+++ weather.js
@@ -1,25 +1,41 @@
  // Functions 
- async function getWeather(apikey, zip) {
     // Replace this with your own API key!
-    const apiKey = '1c4e590826ba3ca40333880acb9b0ddd'
-    const units = 'imperial'
-    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    
-    const response = await fetch(path)
-    const data = await response.json()
 
     // new object iwth more descriptive names
-    const weatherData = {
-      city: data.name,
-      temperature: data.main.temp,
-      description: data.weather[0].description,
-      humidity: data.main.humidity,
-      windSpeed: data.wind.speed,
-    };
+    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+    const API_KEY = '1c4e590826ba3ca40333880acb9b0ddd';
+    
+    async function getWeatherByZip(zip, unit = 'imperial') {
+      const url = `${BASE_URL}zip=${zip}&units=${unit}&appid=${API_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return formatWeatherData(data);
+    }
+    
+    async function getWeatherByCity(city, unit = 'imperial') {
+      const url = `${BASE_URL}q=${city}&units=${unit}&appid=${API_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return formatWeatherData(data);
+    }
+    
+    async function getWeatherByGeo(coords, unit = 'imperial') {
+      const url = `${BASE_URL}lat=${coords.lat}&lon=${coords.lon}&units=${unit}&appid=${API_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return formatWeatherData(data);
+    }
+    
+    function formatWeatherData(data) {
+      return {
+        city: data.name,
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+      };
 
-    console.log(weatherData)
-    return weatherData;
-
-  }
-
-  export { getWeather }
+      console.log(formatWeatherData(data));
+    }
+    
+    export { getWeatherByZip, getWeatherByCity, getWeatherByGeo };
