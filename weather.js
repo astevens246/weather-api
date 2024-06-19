182321cd2056173fe38dfe95e2e9949b4acc0f05
Allen Stevens <allen.stevens@students.dominican.edu>
2024-06-19 15:21:52 -0700

    completed stretch challenges

--- weather.js
+++ weather.js
@@ -1,41 +1,54 @@
- // Functions 
-    // Replace this with your own API key!
+class Weather {
+  constructor(apikey) {
+    this.apikey = apikey;
+    this.BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+  }
 
-    // new object iwth more descriptive names
-    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-    const API_KEY = '1c4e590826ba3ca40333880acb9b0ddd';
-    
-    async function getWeatherByZip(zip, unit = 'imperial') {
-      const url = `${BASE_URL}zip=${zip}&units=${unit}&appid=${API_KEY}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      return formatWeatherData(data);
-    }
-    
-    async function getWeatherByCity(city, unit = 'imperial') {
-      const url = `${BASE_URL}q=${city}&units=${unit}&appid=${API_KEY}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      return formatWeatherData(data);
-    }
-    
-    async function getWeatherByGeo(coords, unit = 'imperial') {
-      const url = `${BASE_URL}lat=${coords.lat}&lon=${coords.lon}&units=${unit}&appid=${API_KEY}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      return formatWeatherData(data);
-    }
-    
-    function formatWeatherData(data) {
-      return {
-        city: data.name,
-        temperature: data.main.temp,
-        description: data.weather[0].description,
-        humidity: data.main.humidity,
-        windSpeed: data.wind.speed,
-      };
+  async getWeatherByZip(zip, unit = 'imperial') {
+    const url = `${this.BASE_URL}zip=${zip}&units=${unit}&appid=${this.apikey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return this.formatWeatherData(data);
+  }
 
-      console.log(formatWeatherData(data));
-    }
-    
-    export { getWeatherByZip, getWeatherByCity, getWeatherByGeo };
+  async getWeatherByCity(city, unit = 'imperial') {
+    const url = `${this.BASE_URL}q=${city}&units=${unit}&appid=${this.apikey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return this.formatWeatherData(data);
+  }
+
+  async getWeatherByGeo(coords, unit = 'imperial') {
+    const url = `${this.BASE_URL}lat=${coords.lat}&lon=${coords.lon}&units=${unit}&appid=${this.apikey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return this.formatWeatherData(data);
+  }
+
+  async getWeatherById(id, unit = 'imperial') {
+    const url = `${this.BASE_URL}id=${id}&units=${unit}&appid=${this.apikey}`;
+    return this.fetchWeatherData(url);
+  }
+
+
+  getPeriodicWeatherUpdates(zip, callback, interval, unit = 'imperial') {
+    setInterval(async () => {
+      const weatherData = await this.getWeatherByZip(zip, unit);
+      callback(weatherData);
+    }, interval);
+  }
+
+  formatWeatherData(data) {
+    return {
+      city: data.name,
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+    };
+  }
+}
+
+const weather = new Weather('1c4e590826ba3ca40333880acb9b0ddd');
+weather.getPeriodicWeatherUpdates('94109', data => console.log(data), 60000);
+export default Weather;
